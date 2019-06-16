deaee057af02ca7030fe0fe0a1b0317fb9b8d752
Keenan Thomson <keenanthomson@gmail.com>
2019-06-15 23:34:48 -0400

    Master updated and webpack + client working together

--- server/app.js
+++ server/app.js
@@ -2,12 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-<<<<<<< HEAD
-app.use(express.static('client'));
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
-=======
 app.use(express.static('react-client/dist'));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
->>>>>>> tmpWork
