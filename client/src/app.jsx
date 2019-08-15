5849e703349c9ccee621894bec9e7ea64dfcedf9
Keenan Thomson <keenanthomson@gmail.com>
2019-08-15 18:21:16 -0400

    Make progress on basic calculations & styling

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -10,11 +10,15 @@ class App extends React.Component {
       target: null,
       entry: null,
       stop: null,
+      risk: null,
+      portfolio: null,
       XBTUSD: 'live price engine loading...'
     };
     this.targetChange = this.targetChange.bind(this);
     this.entryChange = this.entryChange.bind(this);
     this.stopChange = this.stopChange.bind(this);
+    this.portfolioChange = this.portfolioChange.bind(this);
+    this.riskChange = this.riskChange.bind(this);
   }
 
   componentDidMount() {
@@ -51,20 +55,43 @@ class App extends React.Component {
     })
   }
 
-  renderCalc() {
+  portfolioChange(e) {
+    this.setState ({
+      portfolio: e.target.value
+    })
+  }
+
+  riskChange(e) {
+    this.setState ({
+      risk: e.target.value / 100 // convert percentage to decimal
+    })
+  }
+
+  renderR() {
     if (this.state.target && this.state.entry && this.state.stop) {
-      return (`R: ${(this.state.target-this.state.entry) / (this.state.entry-this.state.stop)}`)
+      return (
+        <div>
+      <h5>R: {Math.floor(this.state.target-this.state.entry) / (this.state.entry-this.state.stop)}</h5>
+      <h5>Risk: </h5>
+      </div>
+      )
     
       } else {
-      return (`Add entry, target, and stop for Portfolio Risk and R...`)
+      return (<h5 className="warn"><i>Add entry, target, and stop.</i></h5>)
     };
   };
+
+  renderPositionSize() {
+    if (this.state.portfolio && this.state.risk) {
+      return(<h5>Position Size = {(this.state.portfolio * (1-(this.state.entry/this.state.stop)) / this.state.risk)}</h5>)
+    }
+  }
 
   render() {
     return (
       <div className="entries-calc">
         <div className="entries-calc-inner">
-          <h3>XBT/USD: {this.state.XBTUSD}</h3>
+          <h3 className="title">XBT/USD: {this.state.XBTUSD}</h3>
           <table className="entries">
             <tbody>
               <tr>
@@ -81,9 +108,20 @@ class App extends React.Component {
               </tr>
             </tbody>
           </table>
-          <h5>
-            {this.renderCalc()}
-          </h5>
+          {this.renderR()}
+          {this.renderPositionSize()}
+          <table>
+            <tbody>
+            <tr>
+                <td>Portfolio Size: </td>
+                <td><input type="text" autoComplete="off" onChange={this.portfolioChange}></input></td>
+              </tr>
+              <tr>
+                <td>Portfolio Risk: </td>
+                <td><input type="text" autoComplete="off" onChange={this.riskChange}></input></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     )
