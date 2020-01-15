d38b4e6243e2a503954542dc5b3e0160c37bf941
Keenan Thomson <keenanthomson@gmail.com>
2020-01-15 08:00:07 -0500

    Work on chart logic

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -123,6 +123,7 @@ class App extends React.Component {
       <div>
         <TradingViewWidget 
         symbol="BITMEX:XBTUSD"
+        container_id= "technical-analysis"
         hide_side_toolbar="false"
         studies="[]"
         theme="Dark"
@@ -132,6 +133,39 @@ class App extends React.Component {
         hide_legend="true"
         />
       </div>
+  {/* <div class="tradingview-widget-container">
+  <div id="technical-analysis"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "container_id": "technical-analysis",
+  "width": 998,
+  "height": 610,
+  "symbol": "AAPL",
+  "interval": "D",
+  "timezone": "exchange",
+  "theme": "Light",
+  "style": "1",
+  "toolbar_bg": "#f1f3f6",
+  "withdateranges": true,
+  "hide_side_toolbar": false,
+  "allow_symbol_change": true,
+  "save_image": false,
+  "studies": [
+    "ROC@tv-basicstudies",
+    "StochasticRSI@tv-basicstudies",
+    "MASimple@tv-basicstudies"
+  ],
+  "show_popup_button": true,
+  "popup_width": "1000",
+  "popup_height": "650",
+  "locale": "en"
+}
+  );
+  </script> */}
+{/* </div> */}
     </div>
     )
   }
