66f95b67835e431a25d439f0b79e7111b613022e
Keenan Thomson <keenanthomson@gmail.com>
2020-03-24 08:49:49 -0400

    Get jest working

--- test/exchangeSelector.test.js
+++ test/exchangeSelector.test.js
@@ -1,4 +1,22 @@
-import ExchangeSelector from './client/src/exchangeSelector.jsx';
+// import ExchangeSelector from './client/src/exchangeSelector.jsx';
 
 let wrapper;
 
+// beforeEach(() => {
+//   wrapper = shallow(<ExchangeSelector /> );
+// });
+
+describe('Test Test', () => {
+  function sum(a,b){
+    return a+b;
+ }
+ function minus (a,b){
+    return a-b;
+ }
+ it('Testing sum',()=>{
+    expect(sum(2,2)).toBe(4);
+ })
+ it('Testing minus' ,() => {
+    expect(minus(5,2)).toBe(3);
+ });
+});
