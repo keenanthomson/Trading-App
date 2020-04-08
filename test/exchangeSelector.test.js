54a81fefdf12fcab0d49ac740b3fdaf513ffde0b
Keenan Thomson <keenanthomson@gmail.com>
2020-04-08 10:53:19 -0400

    Finish exchangeSelector tests

--- test/exchangeSelector.test.js
+++ test/exchangeSelector.test.js
@@ -7,15 +7,20 @@ describe('<ExchangeSelector/> Component', () => {
     activeExchange: 'Bitmex',
   }
   const wrapper = shallow(<ExchangeSelector {...elementProps}/> );
+  const event = { preventDefault: () => {} };
+  const spy = jest.spyOn(event, 'preventDefault');
 
   it('should render one <DropdownButton>', () => {
     expect(wrapper.find('DropdownButton')).toHaveLength(1);
   });
 
-  it('should prevent default onMouseDown/Up', () => {
-    const event = { preventDefault: () => {} };
-    const spy = jest.spyOn(event, 'preventDefault');
+  it('should prevent default onMouseDown', () => {
     wrapper.find('ButtonToolbar').simulate('mouseDown', event);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should prevent default onMouseUp', () => {
+    wrapper.find('ButtonToolbar').simulate('mouseUp', event);
     expect(spy).toHaveBeenCalled();
   });
 
