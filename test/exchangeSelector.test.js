5898af5b2b7c2016cf3b58fa5197a5cd7a31c986
Keenan Thomson <keenanthomson@gmail.com>
2020-04-08 10:46:21 -0400

    test preventDefault firing

--- test/exchangeSelector.test.js
+++ test/exchangeSelector.test.js
@@ -1,20 +1,22 @@
 import ExchangeSelector from '../client/src/exchangeSelector.jsx';
 
-const elementProps = {
-  exchanges: ['Bitmex', 'ByBit'],
-  activeExchange: 'Bitmex',
-}
-const wrapper = shallow(<ExchangeSelector {...elementProps} /> );
-
 describe('<ExchangeSelector/> Component', () => {
+
+  const elementProps = {
+    exchanges: ['Bitmex', 'ByBit'],
+    activeExchange: 'Bitmex',
+  }
+  const wrapper = shallow(<ExchangeSelector {...elementProps}/> );
+
   it('should render one <DropdownButton>', () => {
     expect(wrapper.find('DropdownButton')).toHaveLength(1);
   });
 
-  it('should prevent default onMouseEnter/Leave', () => {
-    const event = jest.spyOn(ExchangeSelector.prototype, 'preventDefault');
-    wrapper.find('button').simulate('onmousedown');
-    expect(event).toHaveBeenCalled();
+  it('should prevent default onMouseDown/Up', () => {
+    const event = { preventDefault: () => {} };
+    const spy = jest.spyOn(event, 'preventDefault');
+    wrapper.find('ButtonToolbar').simulate('mouseDown', event);
+    expect(spy).toHaveBeenCalled();
   });
 
 });
